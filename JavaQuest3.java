14691c67c6527e28daefe919b5b6a6b9307dab34
Francis Chang <francis.hongkong@gmail.com>
2025-04-10 23:14:08 +0800

    update 20250410

---
+++ JavaQuest3.java
@@ -0,0 +1,31 @@
+public class JavaQuest3 {
+  public static void main(String[] args) {
+    int a = 10;
+    if (a == 9) {
+      System.out.println("a is equals to 9");
+    } else if (a >= 0 && a < 9) {
+      System.out.println("a is between 0 and 8");
+    // how about a > 9?
+    } else if (a > 9){
+      System.out.println("a is larger than 9");
+    } else {
+      System.out.println("a < 0");
+    }
+
+    String str = "I love Java. I love coding";
+    int strLength = str.length();
+    System.out.println("No. of Words: " + strLength);
+
+    int strWord = strLength;
+    if (strWord == 27) {
+      System.out.println("str length = 27");
+    } else if (strWord < 27) { // code here ... add the condition
+      System.out.println("str length < 27");
+    } else if (strWord > 27) { // code here ... complete the code
+      System.out.println("str length > 27");
+    }
+
+
+    }
+  }
+
